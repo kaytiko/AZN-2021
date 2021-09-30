ae3da9334af40340e83d2df4dea492a0053d682b
Baraya <katrin3991@gmail.com>
2021-09-30 20:23:14 +0700

    правки

--- source/assets/js/button-animation.js
+++ source/assets/js/button-animation.js
@@ -1,7 +1,5 @@
 function buttonSplit (button) {
     let buttonAll = document.querySelectorAll(button);
-    console.log(buttonAll);
-
     buttonAll.forEach(button => button.innerHTML = '<span>' + button.textContent.trim().split('').join('</span><span>') + '</span>')
 }
 
